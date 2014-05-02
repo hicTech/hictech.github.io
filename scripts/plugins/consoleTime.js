a630960018a8d52949b4651d287326ffeb8a3f37
Marco <hictechsrl@gmail.com>
2014-05-02 19:43:48 +0200

    a commit

--- scripts/plugins/consoleTime.js
+++
@@ -1,9 +0,0 @@
-if(navigator.userAgent.indexOf("iPad") != -1 || navigator.userAgent.indexOf("iPhone") != -1  ){
-	console.timers = {};
-	console.time = function(label){
-	  console.timers[label] = new Date().getTime();
-	};
-	console.timeEnd = function(label){
-	   console.log(label+": "+(new Date().getTime()-console.timers[label]));
-	};
-}
